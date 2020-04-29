0426b9dcdd3fdd204f1f8965c2c5155cb226c2ae
grollote <grolleau.annelise@gmail.com>
2020-04-29 11:13:51 +0200

    commencement javascript

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-<script>
 //Variables//
 
     var memory=[];
@@ -7,11 +6,39 @@
     var card=[];
     
 
-
 //Functions//
 
 function start(){
 
+        // Afficher tableau 24 cartes//
+    genererCartes(24);
+    for (var i = 0, c = memory.length; i < c; i++) {
+
+        alert(myArray[i]);
+        }
+    
+    melangerCartes(card);
+    var j=0;
+    var temp=null;
+    for (var i =0; i>card.length; i++){
+        var alea = Math.floor (Math.random () * (i+1));
+        temp=card[i];
+        card[i]=alea[j];
+        alea[j]=temp;
+        }
+
+     
+    afficherCartes(); // Disposer les cartes de manière aléatoire
+}
+function genererCartes(nb_cartes) {
+
+    // tas_de_cartes = ['img-alt1.png', 'img-alt1.png', 'img-alt2.png', ...]
+}
+function retournerCartes() {
 
 }
-</script>
+function afficheCartes() {
+    var board = document.getElementById('board');
+    // ... sur mon tableau aléatoire
+    board.innerHtml += <img id='myimg' src='pictures/card-back.png' data-alt-img='pictures/img-alt2.png'/></div>";
+    // ...
