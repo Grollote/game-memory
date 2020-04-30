16f83acae10e83a740cf93aaa23c49e05c53e591
grollote <grolleau.annelise@gmail.com>
2020-04-30 04:21:34 +0200

    memory fonctionnel

--- javascript.js
+++ javascript.js
@@ -1,51 +1,72 @@
-//  Variables//
+//Variables//
+
+var a = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12] // tableau contenant les 12 paires de cartes
+    .map(p=>[p,Math.random()])//renvoit un nombre aléatoire compris entre 0 et 1
+    .sort((a,b)=>a[1]-b[1])//tri les éléments dans le désordre
+    .map(p=>p[0]) //arrondir
+
+console.log(a)
+
+var music = document.getElementById("myAudio"); 
+var pictures = document.getElementsByTagName('img');//récupère tous les éléments de type <img>
+var eltScore = document.getElementById('score');
+var score = 0;
+var step = 1;
+var p1, p2;
+var timer = null;
 
 
-var deck = [];
-var element="";
-var stroke = 0;
-var card=[];
-    
+for(let i =0; i < pictures.length; i++){
+    pictures[i].src2 = 'pictures/img-alt' + a[i] + '.png';
+}
 
-
-//Functions//
-
-
-document.addEventListener('click', function(e){
-     
+document.addEventListener('click',function(e){
+    switch(step){
+        case 1 :
+            if (e.target.tagName=='IMG'){
+                e.target.src = e.target.src2;
+                p1 = e.target;
+                step = 2;
+            }
+            break;
+        case 2 :
+            if (e.target.tagName=='IMG'){
+                e.target.src = e.target.src2;
+                p2 = e.target;
+                step = 3;
+            }
+            timer = setTimeout(check, 1500);
+            break;
+        case 3 :
+            clearTimeout(timer);
+            check();
+            break;
+     }
 });
-start();
 
-function start(){
-    genererCartes(24); // Afficher tableau 24 cartes
-    melangerCartes(deck); // shuffle
-    afficherCartes();
-    console.log(deck)//afficherCartes(); Disposer les cartes de manière aléatoire//
-}
-function genererCartes(nb_cartes) {
-    for (let index = 1; index <= 12; index++) {
-        deck.push('img-alt'+index+'.png');
-        deck.push('img-alt'+index+'.png');
+function check(){
+    if (p1.src2==p2.src2){
+        p1.replaceWith(document.createElement('span'))
+        p2.replaceWith(document.createElement('span'))
+        score += 50;
     }
-    
-}
-function melangerCartes(card){
-    var temp=null;
-    for (var i =0; i<card.length; i++){
-        var alea = Math.floor (Math.random () * (i+1));
-        temp=card[i];
-        card[i]=card[alea];
-        card[alea]=temp;
+    else{
+        p2.src = p1.src = 'pictures/img-alt0.png';
+        score = Math.max(0, score-30);//perte de 30 pts lorsqu'il s'agit d'une mauvaise paire//
+    }
+    step = 1;
+    eltScore.textContent = score ;
+    if (document.getElementsByTagName('img').length==0){
+        eltScore.textContent += " You win !";
+        location.reload()//relance le jeu
     }
 }
-function retournerCartes() {
 
-}
-function afficherCartes() {
-    var board = document.getElementById('board');
-    // ... sur mon tableau aléatoire
-    var card = document.getElementById('deck').getAttribute(data-alt-img);
+function playAudio() {
+    music.play(); 
+  } 
+  
+function pauseAudio() { 
+    music.pause(); 
+  } 
 
-   board.innerHtml += "<img id='img' src='pictures/card-back.png' data-alt-img='pictures/img-alt2.png'/>";
-    
-}
